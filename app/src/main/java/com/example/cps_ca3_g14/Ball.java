6f3dea0fac072d99c0147ad5bc0dfdad83429118
moein.shafi <mosafer.moein@gmail.com>
2021-03-26 08:35:59 +0430

    Add ball and move it staticly.

--- app/src/main/java/com/example/cps_ca3_g14/Ball.java
+++ app/src/main/java/com/example/cps_ca3_g14/Ball.java
@@ -1,5 +1,42 @@
 package com.example.cps_ca3_g14;
 
-public class Ball{
+import android.widget.ImageView;
+
+public class Ball {
+
+    float x, y;
+    float xVelocity, yVelocity;
+    float xAcceleration, yAcceleration;
+    float mass;
+    ImageView imageView;
+
+    public Ball(float x, float y, float xVelocity, float yVelocity, float xAcceleration,
+                float yAcceleration, float mass, ImageView imageView) {
+        this.x = x;
+        this.y = y;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.xAcceleration = xAcceleration;
+        this.yAcceleration = yAcceleration;
+        this.mass = mass;
+        this.imageView = imageView;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+        this.imageView = imageView;
+
+    }
+
+    public void updateLocation()
+    {
+        /// TODO: update x and y.
+        this.x += 1;    // just for test
+        this.y += 2;    // just for test
+        this.updateImageLocation();
+    }
+
+    private void updateImageLocation() {
+        imageView.setX(this.x);
+        imageView.setY(this.y);
+    }
 
 }
--- app/src/main/java/com/example/cps_ca3_g14/GyroscopeActivity.java
+++ app/src/main/java/com/example/cps_ca3_g14/GyroscopeActivity.java
@@ -3,12 +3,31 @@ package com.example.cps_ca3_g14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GyroscopeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyroscope);
+    }
+
+    public void startGame(View view) {
+        ImageView ballImage = findViewById(R.id.ball);
+        /// TODO: set random for x, y and others.
+        /// TODO: invisible the start button.
+        Ball ball = new Ball(10, 10, 5, 5, 2,2,1, ballImage);
+        ball.updateLocation();
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+                @Override
+                public void run() { ball.updateLocation();
+                }
+            }, 1, 100);
     }
 }
