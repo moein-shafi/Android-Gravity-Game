15a67c5eb63e3b829c4a262e76d22de561f08537
moein.shafi <mosafer.moein@gmail.com>
2021-03-26 06:14:45 +0430

    Add navigation between pages.

--- app/src/main/java/com/example/cps_ca3_g14/GravityActivity.java
+++ app/src/main/java/com/example/cps_ca3_g14/GravityActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import com.example.cps_ca3_g14.R;
-
 public class GravityActivity extends AppCompatActivity {
 
     @Override
@@ -14,18 +12,3 @@ public class GravityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gravity);
     }
 }
-
-//package com.example.cps_ca3_g14;
-//
-//        import androidx.appcompat.app.AppCompatActivity;
-//
-//        import android.os.Bundle;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//}
--- app/src/main/java/com/example/cps_ca3_g14/MainActivity.java
+++ app/src/main/java/com/example/cps_ca3_g14/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.cps_ca3_g14;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void useGyroscope(View view) {
+        Intent intent = new Intent(this, GyroscopeActivity.class);
+        startActivity(intent);
+    }
+    public void useGravity(View view) {
+        Intent intent = new Intent(this, GravityActivity.class);
+        startActivity(intent);
+    }
+
 }
